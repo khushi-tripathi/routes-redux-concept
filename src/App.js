d10d386a3a4252922455107955adaa44b6ff0e44
Khushi Tripathi <khushitripathi004@gmail.com>
2026-01-08 15:04:30 +0530

    Summary: Set routing to our application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { createBrowserRouter } from "react-router";
+// import {  } from "react-router/dom";
+import Home from "./pages.js/Home";
+import About from "./pages.js/About";
+import Contact from "./pages.js/Contact";
+import { RouterProvider } from "react-router/dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+  ]);
   return (
-    <div className="App">
-      <h1>Routing </h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+      <div className="App">
+        <h1>Routing </h1>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <button> Home</button>
+      <button>About </button>
+      <button>Contact</button>
+    </div>
+  );
+}
+
+export default NavBar;
