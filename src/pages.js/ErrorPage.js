3f5bc99265e20e8c65751777c67817e9422a76cb
Khushi Tripathi <khushitripathi004@gmail.com>
2026-01-08 17:43:55 +0530

    Summary: Use useNavigate hook in error file

--- src/pages.js/ErrorPage.js
+++ src/pages.js/ErrorPage.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 function ErrorPage() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/"); // navigate one page back if we use -1, -2 for 2 page back
+    }, 3000);
+  }, []);
   return <h1>Something Went Wrong!!</h1>;
 }
 
